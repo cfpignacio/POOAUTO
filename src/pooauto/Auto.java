c96121473f5306e6af37d3d0f49f3747a1d0cf85
Ignaciom <ignacio.camillucci@gmail.com>
2020-08-24 19:39:58 -0300

    Initial commit

---
+++ src/pooauto/Auto.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pooauto;
+
+/**
+ *
+ * @author Notebook
+ */
+public class Auto {
+    
+    boolean encendido = false;
+    String Marca ;
+    
+    public void Encender(){
+        encendido = true;
+    }
+    
+    public void Apagar(){
+        encendido = false;
+    }
+    
+    
+}
